59e385dcfe12baa1fb98347c64dd8964a72882d8
Logan G <logan.giese@eagles.oc.edu>
2020-07-10 13:31:39 -0600

    Add Message type to hold pub/sub message info

---
+++ Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PublishSubscribeBroker
+{
+    /// <summary>
+    /// Serializable message object containing information for a published message in a pub/sub system
+    /// </summary>
+    [Serializable]
+    public class Message<T>
+    {
+        /// <summary>
+        /// The name or ID of the message publisher
+        /// </summary>
+        public string Publisher { get; set; }
+
+        /// <summary>
+        /// The topic where the message was published
+        /// </summary>
+        public string Topic { get; set; }
+
+        /// <summary>
+        /// The date and time when the message was published
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// The content of the message
+        /// </summary>
+        public T Content { get; set; }
+
+    }
+}
