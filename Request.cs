43f72446592b80c16f96e01652e155672e15e9ab
Logan G <logan.giese@eagles.oc.edu>
2020-07-11 13:16:34 -0600

    Add pub/sub communication data objects

---
+++ Request.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PublishSubscribeBroker
+{
+    /// <summary>
+    /// Base class for client requests in a pub/sub system, where a request has a type to distinguish its contents
+    /// </summary>
+    [Serializable]
+    public class Request
+    {
+        /// <summary>
+        /// Enumerator for determining the type/operation of a request
+        /// </summary>
+        public enum RequestType { SUBSCRIBE, UNSUBSCRIBE, PUBLISH, CREATE_TOPIC, LIST_TOPICS }
+
+        /// <summary>
+        /// The type/operation of this request
+        /// </summary>
+        public RequestType Type { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized request for a subscription request, where a client subscribes or unsubscribes to a topic
+    /// </summary>
+    [Serializable]
+    public class SubscriptionRequest : Request
+    {
+        /// <summary>
+        /// The topic to which the client wants to subscribe or unsubscribe
+        /// </summary>
+        public Topic Topic { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized request for a publish request, where a client publishes a message to a topic
+    /// </summary>
+    /// <typeparam name="T">The type of the message contents (determined by protocol)</typeparam>
+    [Serializable]
+    public class PublishRequest<T> : Request
+    {
+        /// <summary>
+        /// The topic to which the client wants to publish
+        /// </summary>
+        public Topic Topic { get; set; }
+
+        /// <summary>
+        /// The message/content that the client wants to publish
+        /// </summary>
+        public Message<T> Message { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized request for a topic creation request, where a client creates a new topic
+    /// </summary>
+    [Serializable]
+    public class CreateTopicRequest : Request
+    {
+        /// <summary>
+        /// The new topic that the client wishes to create
+        /// </summary>
+        public Topic Topic { get; set; }
+    }
+}
---
+++ Response.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PublishSubscribeBroker
+{
+    /// <summary>
+    /// Base class for server responses in a pub/sub system, where a response has a type to distinguish its contents
+    /// </summary>
+    [Serializable]
+    public class Response
+    {
+        /// <summary>
+        /// Enumerator for determining the type/operation of a response
+        /// </summary>
+        public enum ResponseType { INFO, NEW_MESSAGE, LIST_TOPICS }
+
+        /// <summary>
+        /// The type/operation of this response
+        /// </summary>
+        public ResponseType Type { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized response for when the server is sending simple text information
+    /// </summary>
+    [Serializable]
+    public class InfoResponse : Response
+    {
+        /// <summary>
+        /// The text information sent in the response
+        /// </summary>
+        public string Text { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized response for when the server is sending a newly-published message from a subscribed topic
+    /// </summary>
+    /// <typeparam name="T">The type of the message contents (determined by protocol)</typeparam>
+    [Serializable]
+    public class NewMessageResponse<T> : Response
+    {
+        /// <summary>
+        /// The topic from which the new message originated
+        /// </summary>
+        public Topic Topic { get; set; }
+
+        /// <summary>
+        /// The message/content that is being delivered to the client
+        /// </summary>
+        public Message<T> Message { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized response for when the server is sending a list of all active topics
+    /// </summary>
+    [Serializable]
+    public class ListTopicsResponse : Response
+    {
+        /// <summary>
+        /// The list of active topics available for the client to subscribe to
+        /// </summary>
+        public List<Topic> Topics { get; set; }
+    }
+}
