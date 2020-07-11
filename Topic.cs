7dd993dfd154243072300c4dcb069b194dd65736
Logan G <logan.giese@eagles.oc.edu>
2020-07-11 13:22:41 -0600

    Add topic data objects

---
+++ Topic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PublishSubscribeBroker
+{
+    /// <summary>
+    /// Data object containing information about a topic
+    /// </summary>
+    [Serializable]
+    public class Topic
+    {
+        /// <summary>
+        /// The readable name of the topic
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The unique internal ID of the topic
+        /// </summary>
+        public Guid ID { get; set; }
+    }
+
+    /// <summary>
+    /// Specialized Topic for the broker that also contains information about current subscribers
+    /// </summary>
+    public class BrokerTopic : Topic
+    {
+        /// <summary>
+        /// The list of subscribers that are currently subscribed to this topic
+        /// </summary>
+        public List<Subscriber> Subscribers;
+    }
+}
